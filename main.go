903214377c4dc5b7757273ee342ef97f2c9cd87e
SoarYu <349815610@qq.com>
2022-09-07 23:28:04 +0800

    update server1.3.1

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	nacosService "nacos-grpc-service/server"
+	//nacosService "nacos-grpc-service/server"
+	nacosService1_3_1 "nacos-grpc-service/server1.4.4"
 	"sync"
 )
 
@@ -10,10 +11,10 @@ func main() {
 
 	wg.Add(1) // 因为有两个动作，所以增加2个计数
 	go func() {
-		nacosService.Serve("demo.go", []string{"192.168.66.146"}, "192.168.66.146", "106.52.77.111", 8461, "")
+		//nacosService1_3_1.Serve("demo.go", []string{"192.168.66.146"}, "192.168.66.146", "106.52.77.111", 8461, "")
+		nacosService1_3_1.Serve("demo.go", []string{"192.168.66.146"}, "106.52.77.111", "106.52.77.111", 8461, "")
 		wg.Done() // 操作完成，减少一个计数
 	}()
-
 
 	wg.Wait()
 
--- server1.4.4/nacosService1.4.4.go
+++ server1.4.4/nacosService1.4.4.go
@@ -8,6 +8,7 @@ import (
 	"github.com/nacos-group/nacos-sdk-go/common/constant"
 	"github.com/nacos-group/nacos-sdk-go/vo"
 	"google.golang.org/grpc"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"nacos-grpc-service/pb/person"
 	"net"
 	"strconv"
@@ -24,7 +25,7 @@ func (this *Children) SayHello(ctx context.Context, p *person.Person) (*person.P
 }
 
 func Serve(serviceName string, serverAddr []string, localAddr string, npsAddr string, servicePort uint64, cluster string) {
-	RegisterNacos(serviceName, serverAddr, npsAddr, servicePort, cluster)
+	RegisterNacos()
 	//////////////////////以下为 grpc 服务远程调用//////////////////////////////
 
 	// 1.初始化 grpc 对象,
@@ -47,36 +48,12 @@ func Serve(serviceName string, serverAddr []string, localAddr string, npsAddr st
 	grpcServer.Serve(listener)
 }
 
-func ServeNotRegis(localAddr string, servicePort uint64) {
-	// RegisterNacos(serviceName, serverAddr, npsAddr, servicePort, cluster)
-	//////////////////////以下为 grpc 服务远程调用//////////////////////////////
-
-	// 1.初始化 grpc 对象,
-	grpcServer := grpc.NewServer()
-
-	// 2.注册服务
-	person.RegisterHelloServer(grpcServer, new(Children))
-
-	// 3.设置监听, 指定 IP/port
-	listener, err := net.Listen("tcp", localAddr+":"+strconv.FormatUint(servicePort, 10))
-	if err != nil {
-		fmt.Println("Listen err:", err)
-		return
-	}
-	defer listener.Close()
-
-	fmt.Println(strconv.FormatUint(servicePort, 10) + "服务启动... ")
-
-	// 4. 启动服务
-	grpcServer.Serve(listener)
-}
-
-var nacos_client clients.NewNamingClient
+var nacos_client naming_client.INamingClient
 
 func initServerClient() {
 	sc := []constant.ServerConfig{
 		{
-			IpAddr: "192.168.66.146",
+			IpAddr: "106.52.77.111",
 			Port:   8848,
 		},
 	}
@@ -122,66 +99,81 @@ func initServerClient() {
 
 }
 
-func RegisterNacos(serviceName string, serverAddr []string, npsAddr string, servicePort uint64, cluster string) {
+//func RegisterNacos(serviceName string, serverAddr []string, npsAddr string, servicePort uint64, cluster string) {
+func RegisterNacos() {
 
-	success, err := namingClient.RegisterInstance(vo.RegisterInstanceParam{
-		Ip:          npsAddr,
-		Port:        servicePort,
-		ServiceName: serviceName,
+	//success, _ := nacos_client.RegisterInstance(vo.RegisterInstanceParam{
+	//	Ip:          npsAddr,
+	//	Port:        servicePort,
+	//	ServiceName: serviceName,
+	//	Weight:      10,
+	//	Enable:      true,
+	//	Healthy:     true,
+	//	Ephemeral:   true,
+	//	Metadata:    map[string]string{"idc": "shanghai"},
+	//	ClusterName: cluster, // 默认值DEFAULT
+	//	GroupName:   "",      // 默认值DEFAULT_GROUP11
+	//})
+
+	//if !success {
+	//	return
+	//} else {
+	//	fmt.Println("namingClient.RegisterInstance Success")
+	//}
+
+	ExampleServiceClient_RegisterServiceInstance(nacos_client, vo.RegisterInstanceParam{
+		Ip:          "106.52.77.111",
+		Port:        8461,
+		ServiceName: "demo.go",
 		Weight:      10,
 		Enable:      true,
 		Healthy:     true,
 		Ephemeral:   true,
 		Metadata:    map[string]string{"idc": "shanghai"},
-		ClusterName: cluster, // 默认值DEFAULT
-		GroupName:   "",      // 默认值DEFAULT_GROUP11
 	})
-	if !success {
-		return
-	} else {
-		fmt.Println("namingClient.RegisterInstance Success")
-	}
 }
 
+//
 func ExampleServiceClient_RegisterServiceInstance(client naming_client.INamingClient, param vo.RegisterInstanceParam) {
 	success, _ := client.RegisterInstance(param)
 	fmt.Printf("RegisterServiceInstance,param:%+v,result:%+v \n\n", param, success)
 }
 
-func ExampleServiceClient_DeRegisterServiceInstance(client naming_client.INamingClient, param vo.DeregisterInstanceParam) {
-	success, _ := client.DeregisterInstance(param)
-	fmt.Printf("DeRegisterServiceInstance,param:%+v,result:%+v \n\n", param, success)
-}
-
-func ExampleServiceClient_GetService(client naming_client.INamingClient, param vo.GetServiceParam) {
-	service, _ := client.GetService(param)
-	fmt.Printf("GetService,param:%+v, result:%+v \n\n", param, service)
-}
-
-func ExampleServiceClient_SelectAllInstances(client naming_client.INamingClient, param vo.SelectAllInstancesParam) {
-	instances, _ := client.SelectAllInstances(param)
-	fmt.Printf("SelectAllInstance,param:%+v, result:%+v \n\n", param, instances)
-}
-
-func ExampleServiceClient_SelectInstances(client naming_client.INamingClient, param vo.SelectInstancesParam) {
-	instances, _ := client.SelectInstances(param)
-	fmt.Printf("SelectInstances,param:%+v, result:%+v \n\n", param, instances)
-}
-
-func ExampleServiceClient_SelectOneHealthyInstance(client naming_client.INamingClient, param vo.SelectOneHealthInstanceParam) {
-	instances, _ := client.SelectOneHealthyInstance(param)
-	fmt.Printf("SelectInstances,param:%+v, result:%+v \n\n", param, instances)
-}
-
-func ExampleServiceClient_Subscribe(client naming_client.INamingClient, param *vo.SubscribeParam) {
-	client.Subscribe(param)
-}
-
-func ExampleServiceClient_UnSubscribe(client naming_client.INamingClient, param *vo.SubscribeParam) {
-	client.Unsubscribe(param)
-}
-
-func ExampleServiceClient_GetAllService(client naming_client.INamingClient, param vo.GetAllServiceInfoParam) {
-	service, _ := client.GetAllServicesInfo(param)
-	fmt.Printf("GetAllService,param:%+v, result:%+v \n\n", param, service)
-}
+//
+//func ExampleServiceClient_DeRegisterServiceInstance(client naming_client.INamingClient, param vo.DeregisterInstanceParam) {
+//	success, _ := client.DeregisterInstance(param)
+//	fmt.Printf("DeRegisterServiceInstance,param:%+v,result:%+v \n\n", param, success)
+//}
+//
+//func ExampleServiceClient_GetService(client naming_client.INamingClient, param vo.GetServiceParam) {
+//	service, _ := client.GetService(param)
+//	fmt.Printf("GetService,param:%+v, result:%+v \n\n", param, service)
+//}
+//
+//func ExampleServiceClient_SelectAllInstances(client naming_client.INamingClient, param vo.SelectAllInstancesParam) {
+//	instances, _ := client.SelectAllInstances(param)
+//	fmt.Printf("SelectAllInstance,param:%+v, result:%+v \n\n", param, instances)
+//}
+//
+//func ExampleServiceClient_SelectInstances(client naming_client.INamingClient, param vo.SelectInstancesParam) {
+//	instances, _ := client.SelectInstances(param)
+//	fmt.Printf("SelectInstances,param:%+v, result:%+v \n\n", param, instances)
+//}
+//
+//func ExampleServiceClient_SelectOneHealthyInstance(client naming_client.INamingClient, param vo.SelectOneHealthInstanceParam) {
+//	instances, _ := client.SelectOneHealthyInstance(param)
+//	fmt.Printf("SelectInstances,param:%+v, result:%+v \n\n", param, instances)
+//}
+//
+//func ExampleServiceClient_Subscribe(client naming_client.INamingClient, param *vo.SubscribeParam) {
+//	client.Subscribe(param)
+//}
+//
+//func ExampleServiceClient_UnSubscribe(client naming_client.INamingClient, param *vo.SubscribeParam) {
+//	client.Unsubscribe(param)
+//}
+//
+//func ExampleServiceClient_GetAllService(client naming_client.INamingClient, param vo.GetAllServiceInfoParam) {
+//	service, _ := client.GetAllServicesInfo(param)
+//	fmt.Printf("GetAllService,param:%+v, result:%+v \n\n", param, service)
+//}
